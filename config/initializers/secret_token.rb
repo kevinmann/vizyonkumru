1e274350cb4ad69cd8f525be751c360528ed802a
Kevin Mann <k@mann.ly>
2012-12-22 22:32:28 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vizyon::Application.config.secret_token = 'b1e72608591181fde870a81cc9524d818d273a58ac3343955831f3b3d88abeee556c33577c396160f581ebfa7a1df5f4b36dd714e7f6a0f089442c18c655d177'
